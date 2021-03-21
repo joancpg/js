1010ca03dfcf51df31d815782c67fc9625dc52b5
Your Name <you@example.com>
2021-03-21 20:07:00 +0800

    3/19

---
+++ ext/prime- number(me)/custom.js
@@ -0,0 +1,22 @@
+let isPrime = true;
+
+for (let dividend = 2; dividend <= 10; dividend++) {
+    for (let divisor = 2; divisor < dividend; divisor++) {
+        if (dividend % divisor == 0) {
+            isPrime = false;
+        }
+    }
+    if (isPrime !== true) {
+        console.log(`${dividend}不是質數`);
+        // isPrime = true;
+
+
+    } else {
+        console.log(`${dividend}是質數`)
+    }
+
+
+}
+
+
+  // 2不會跑到
